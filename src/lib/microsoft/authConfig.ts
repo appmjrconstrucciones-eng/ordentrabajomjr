9af5893355c6e6c7c3be36195147c9dac70df7fb
Tatan <jdcha24@gmail.com>
2026-05-13 20:42:11 -0600

    fix: implementación de auth-blank redirect para MSAL

--- src/lib/microsoft/authConfig.ts
+++ src/lib/microsoft/authConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: process.env.NEXT_PUBLIC_AZURE_CLIENT_ID!,
     authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_TENANT_ID}`,
-    redirectUri: typeof window !== "undefined" ? window.location.origin : "https://ordentrabajomjr.vercel.app",
+    redirectUri: typeof window !== "undefined" ? `${window.location.origin}/auth-blank` : "https://ordentrabajomjr.vercel.app/auth-blank",
   },
   cache: {
     cacheLocation: "sessionStorage",
